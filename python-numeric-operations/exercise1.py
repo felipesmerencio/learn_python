4062ad7f8b7aed5db19ac31a36029a4d887d1306
merencio <felipe.smerencio@gmail.com>
2022-02-01 20:32:23 -0300

    evolving python-numeric-operations

---
+++ python-numeric-operations/exercise1.py
@@ -0,0 +1,36 @@
+## 1
+# print(type('7'))
+# print(type(7))
+# print(type(7.1))
+
+## 2
+# print(isinstance('7', str))
+# print(isinstance(7, int))
+# print(isinstance(7.1, float))
+
+# print(isinstance(7, str))
+# print(isinstance('7', int))
+# print(isinstance('7.1', float))
+
+## 3
+# print(type('7') == str)
+# print(type(7) == int)
+# print(type(7.1) == float)
+
+# print(type(7) == str)
+# print(type('7') == int)
+# print(type('7.1') == float)
+
+## 4
+# x = 'a string'
+# print(type(x))
+# x = 7
+# print(type(x))
+# x = False 
+# print(type(x))
+
+## 5
+first_value = int(input('First Number: '))
+second_value = int(input('Second number: '))
+sum = first_value + second_value
+print("Sum: " + str(sum))
