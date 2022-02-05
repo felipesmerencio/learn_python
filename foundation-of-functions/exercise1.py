3534b486344bcbf73af1f98da047cad261eb3c97
merencio <felipe.smerencio@gmail.com>
2022-02-04 21:11:43 -0300

    working with functions

---
+++ foundation-of-functions/exercise1.py
@@ -0,0 +1,31 @@
+## 1
+# def rocket_parts():
+#     #print("payload, propellant, structure")
+#     return "payload, propellant, structure"
+
+# output = rocket_parts()
+# print(output)
+
+## 2
+# print (any([True, False, False]))
+# print (any([False, False, False]))
+# print (any())
+
+## 3
+# def distance_from_earth(destination):
+#     if destination == "Moon":
+#         return "238,855"
+#     else:
+#         return "Unable to compute to that destination"
+
+# print(distance_from_earth("Moon"))
+
+## 4
+# def days_to_complete(distance, speed):
+#     hours = distance/speed
+#     return hours/24
+
+# total_days = days_to_complete(238855, 75)
+# print(round(total_days))
+
+## 5
